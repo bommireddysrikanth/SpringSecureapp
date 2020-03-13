f54b7b6ebbde7a402f99b57c87316f107f36f8bb
abhia <abhia@LAPTOP-1H82PAS1>
2020-03-13 10:52:08 +0530

    commitong

---
+++ procucev/src/main/java/com/portal/procucev/model/VendorprocucevDocument.java
@@ -0,0 +1,14 @@
+package com.portal.procucev.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vendor_procucev_documents")
+public class VendorprocucevDocument extends Procucev {
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+}
