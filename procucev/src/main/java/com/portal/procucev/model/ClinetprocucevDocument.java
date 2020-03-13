f9598ab7354c42ff83d15b3ecc0c8d7534b47424
abhia <abhia@LAPTOP-1H82PAS1>
2020-03-13 11:02:17 +0530

    commiting new model

---
+++ procucev/src/main/java/com/portal/procucev/model/ClinetprocucevDocument.java
@@ -0,0 +1,10 @@
+package com.portal.procucev.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client_procucev_documents")
+public class ClinetprocucevDocument  extends Procucev {
+	private static final long serialVersionUID = 1L;
+}
